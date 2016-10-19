97e25cd55146e00aefd73e24fa26381fbe11ed8f
Sean Handley <sean.handley@gmail.com>
2016-10-19 13:55:44 +0100

    Use a newer client.

--- alpine/run_container.sh
+++ alpine/run_container.sh
@@ -5,15 +5,16 @@ apk add --update --no-cache python py-pip git python-dev libffi libffi-dev \
     openssl openssl-dev build-base iputils bash curl linux-headers
 
 # Install client
-git clone https://github.com/openstack/python-openstackclient.git /root/client
-cd /root/client
-git fetch --tags
-git checkout tags/2.3.0 -b v2.3.0
-pip install 'python-openstackclient==2.6.0'
+pip install 'python-openstackclient==3.3.0'
 
 # Install tools
 curl -sSL https://raw.githubusercontent.com/gbraad/openstack-tools/master/stack -o /sbin/stack
 chmod 0755 /sbin/stack
+
+# Patch fix for Neutron commands
+#curl -sSL https://gist.githubusercontent.com/seanhandley/b1343a7adc59df1af16bfcda842278aa/raw/8bc4560f90197fe077c178e521b1aa2b9de574d6/session.py -o /root/client/openstackclient/common/session.py
+curl -sSL https://gist.githubusercontent.com/seanhandley/b1343a7adc59df1af16bfcda842278aa/raw/8bc4560f90197fe077c178e521b1aa2b9de574d6/session.py -o /usr/lib/python2.7/site-packages/openstack/session.py
+#curl -sSL https://gist.githubusercontent.com/seanhandley/b1343a7adc59df1af16bfcda842278aa/raw/8bc4560f90197fe077c178e521b1aa2b9de574d6/session.py -o /usr/lib/python2.7/site-packages/openstackclient/common/session.py
 
 # Cleanup
 apk del build-base linux-headers python-dev libffi-dev openssl-dev
